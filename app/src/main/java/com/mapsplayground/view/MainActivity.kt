3f8fb3fc3b79d38b7cb7126dfd80acd3474ca395
Marius Bareiša <m.bareisa@gmail.com>
2021-03-20 11:47:34 +0200

    Add dependency injection

--- app/src/main/java/com/mapsplayground/view/MainActivity.kt
+++ app/src/main/java/com/mapsplayground/view/MainActivity.kt
@@ -3,7 +3,9 @@ package com.mapsplayground.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapsplayground.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/mapsplayground/view/map/MapFragment.kt
+++ app/src/main/java/com/mapsplayground/view/map/MapFragment.kt
@@ -1,19 +1,24 @@
-package com.mapsplayground.view
+package com.mapsplayground.view.map
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.mapsplayground.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainFragment : Fragment() {
+@AndroidEntryPoint
+class MapFragment : Fragment() {
+
+    private val viewModel: MapViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        viewModel
+        return inflater.inflate(R.layout.fragment_map, container, false)
     }
 }
